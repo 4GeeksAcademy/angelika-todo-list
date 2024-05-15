5148aac0aac4d438752fa505858f72202edc6963
AngelikaWebDev <110480648+AngelikaWebDev@users.noreply.github.com>
2024-05-15 21:38:12 +0000

    ajustes en los estilos

--- src/app.js
+++ src/app.js
@@ -42,9 +42,3 @@ window.onload = function() {
     tasks.forEach(task => taskList.appendChild(task));
   }
 };
-
-const numbers = [4, 2, 5, 1, 3];
-numbers.sort(function(a, b) {
-  return a - b;
-});
-console.log(numbers); // [1, 2, 3, 4, 5]
